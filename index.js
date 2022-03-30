69e4de8abb96fa6a4cd5428cad13e45750df0428
ZahraO1 <zahra.masroor6@gmail.com>
2022-03-30 07:40:51 -0400

    Coded web game using js

--- index.js
+++ index.js
@@ -2,7 +2,92 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+
+let direction = null;
+let x = 100;
+let y = 250;
+
+function moveCharacter(){
+    if(direction === 'north'){
+        y++;
+    }
+    if(direction === 'east'){
+        x++;
+    }
+    if(direction === 'south'){
+        y--;
+    }
+    if(direction === 'west'){
+        x--;
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}
+
+setInterval(function(){
+    if(direction === 'north'){
+        y++;
+    }
+    if(direction === 'east'){
+        x++;
+    }
+    if(direction === 'south'){
+        y--;
+    }
+    if(direction === 'west'){
+        x--;
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}, 1)
+//calls the function moveCharacter every
+//millisecond
+setInterval(moveCharacter, 1)
+
+document.addEventListener('keydown', function(e){                
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
+
+document.addEventListener('keyup',function(e){
+    //character stops moving when no key
+    //is pressed
+    direction = null;
+})
+
 
 
 move(newImage('assets/tree.png')).to(200, 450)
